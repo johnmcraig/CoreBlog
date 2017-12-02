4664e80ee7bc852aba3307e211f2603c4bcd4241
johnmcraig <jmcraig32884@gmail.com>
2017-12-02 13:08:48 -0500

    Added DataLibrary and repo

---
+++ CoreBlogDataLibrary/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreBlogDataLibrary
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Permalink { get; internal set; }
+        public Author PostAuthor { get; set; }
+        public string PostContent { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PostDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EditDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+        
+    }
+}
---
+++ CoreBlogDataLibrary/PostRepository.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CoreBlogDataLibrary
+{
+    public class PostRepository
+    {
+        private static List<Post> _posts = new List<Post>();
+        private static int _nextId = 1;
+        private static string path = @"C:\Users\Mark\Desktop\Projects\BlogRepo";
+        private static string fileName = "postList.json";
+
+        public PostRepository()
+        {
+            if(_posts.Count < 1)
+            {
+                LoadFromFile();
+            }
+        }
+
+        public List<Post> List()
+        {
+            return _posts
+                .Where(p => p.PublishDate <= DateTime.Now)
+                .OrderByDescending(p => p.PublishDate)
+                .ToList();
+        }
+
+        public List<Post> ListAll()
+        {
+            return _posts
+                .OrderByDescending(p => p.PostDate)
+                .ToList();
+                
+        }
+
+        public Post GetById(int id)
+        {
+            return _posts
+                .Find(p => p.Id == id);
+        }
+
+        public Post GetByPermalink(string permalink)
+        {
+            return _posts
+                .Find(p => p.Permalink == permalink);
+        }
+
+        public void Add(Post newPost)
+        {
+            newPost.Id = _nextId++;
+            if (newPost.PostAuthor == null)
+            {
+                newPost.PostAuthor = new Author();
+            }
+            _posts.Add(newPost);
+            SaveToFile();
+        }
+
+        private void SaveToFile()
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //manage nuget package under DataLibrary with newton.json to get it to work later.
+            var serializedPosts = JsonConvert.SerializeObject(_posts);
+            File.WriteAllText(Path.Combine(path, fileName), serializedPosts);
+        }
+
+        private void LoadFromFile()
+        {
+            if (!File.Exists(Path.Combine(path, fileName)))
+            {
+                return;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var serializedPosts = File.ReadAllText(Path.Combine(path, fileName));
+            _posts = JsonConvert.DeserializeObject<List<Post>>(serializedPosts);
+        }
+    }
+}
