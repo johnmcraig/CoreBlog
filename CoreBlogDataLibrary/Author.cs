1215268571bd80d38118e7bcc1f416d4bb6bc2e0
johnmcraig <jmcraig32884@gmail.com>
2017-12-04 02:35:51 -0500

    Fixed some issues with controllers

--- CoreBlogDataLibrary/Author.cs
+++ CoreBlogDataLibrary/Author.cs
@@ -13,6 +13,6 @@ namespace CoreBlogDataLibrary
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [DataType(DataType.PhoneNumber)]
-        public string PhoneNumber { get; set; }
+        public string Phonenumber { get; set; }
     }
 }
